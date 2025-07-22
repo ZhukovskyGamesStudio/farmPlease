e29b1ae8d0591e7e5bfbef3518344223bde044d0
MorozVolodya <aragornkingstudio@gmail.com>
2025-07-22 19:12:15 +0300

    hope I didn't crash anything...

---
+++ Assets/Scripts/Managers/YandexGames/YGProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using YG;
+
+public class YGProvider : IYGProvider {
+    public void ShowAdvReward(Action onShown) {
+        string id = Guid.NewGuid().ToString();
+        YG2.RewardedAdvShow(id, onShown);
+    }
+}
