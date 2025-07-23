dd652482d1159c06f6cfa52c1d1e10ac0b55f9f0
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-07-23 22:40:09 +0300

    add inter ads

--- Assets/Scripts/Ads/IAdsProvider.cs
+++ Assets/Scripts/Ads/IAdsProvider.cs
@@ -2,6 +2,7 @@
 
 public interface IAdsProvider {
     public void ShowRewardedAd(string placeId, Action onSuccess, Action onFail);
+    public void ShowInterAd(string placeId, Action onSuccess = null, Action onFail = null);
 
     public bool IsAdsReady();
 }
--- Assets/Scripts/MadPixelHelpers/MadPixelAdsProvider.cs
+++ Assets/Scripts/MadPixelHelpers/MadPixelAdsProvider.cs
@@ -2,17 +2,18 @@
 using MadPixel;
 
 public class MadPixelAdsProvider : IAdsProvider {
-
+    
     private bool _isShowing;
-    private Action _onRewardedShown;
+    private Action _onAdShown;
     private Action _onFail;
     
     public void ShowRewardedAd(string placeId, Action onSuccess, Action onFail) {
         if (_isShowing) {
             return;
+            
         }
 
-        _onRewardedShown = onSuccess;
+        _onAdShown = onSuccess;
         _onFail = onFail;
         _isShowing = true;
         AdsManager.EResultCode code =  AdsManager.ShowRewarded(MadPixelDDOL.Instance.gameObject, OnFinishAds, placeId);
@@ -23,6 +24,22 @@ public class MadPixelAdsProvider : IAdsProvider {
         }
     }
 
+    public void ShowInterAd(string placeId, Action onSuccess = null, Action onFail = null) {
+        if (_isShowing) {
+            return;
+        }
+        
+        _onAdShown = onSuccess;
+        _onFail = onFail;
+        _isShowing = true;
+        AdsManager.EResultCode code = AdsManager.ShowInter(MadPixelDDOL.Instance.gameObject, OnFinishAds, placeId);
+        if (code != AdsManager.EResultCode.OK)
+        {
+            _onFail?.Invoke();
+            _isShowing = false;
+        }
+    }
+    
     public bool IsAdsReady() {
         return AdsManager.Ready();
     }
@@ -31,7 +48,7 @@ public class MadPixelAdsProvider : IAdsProvider {
     {
         if (adShowSuccess)
         {
-            _onRewardedShown?.Invoke();
+            _onAdShown?.Invoke();
         } else {
             _onFail?.Invoke(); 
         }
