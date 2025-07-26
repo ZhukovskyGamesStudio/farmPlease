46844a202319f72d44258c4ad15806062ae7fa16
Dmitriy Krivko <dmkrivko@yandex.ru>
2025-07-26 20:28:54 +0300

    fix rate us

--- Assets/Scripts/RateUs/RateUsDialog.cs
+++ Assets/Scripts/RateUs/RateUsDialog.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using Cysharp.Threading.Tasks;
 using Managers;
@@ -14,20 +15,34 @@ public class RateUsDialog : DialogBase {
         }
         
         _isWaitingReview = true;
+        SendRateUsEvent(5);
         SaveLoadManager.CurrentSave.WasRated = true;
         SaveLoadManager.SaveGame();
         LaunchReviewFlow().Forget();
     }
 
     public void RateBad() {
+        SendRateUsEvent(1);
         SaveLoadManager.CurrentSave.LastTimeRateUsShowed = DateTime.Now.ToString(CultureInfo.InvariantCulture);
         SaveLoadManager.SaveGame();
         CloseByButton();
     }
 
+    public void CloseWithEvent() {
+        SendRateUsEvent(0);
+        CloseByButton();
+    }
     private async UniTask LaunchReviewFlow() {
         await RateUsManager.Instance.Provider.Show();
 
         CloseByButton();
+    }
+
+    private void SendRateUsEvent(int rateResult) {
+        ZhukovskyAnalyticsManager.Instance.SendCustomEvent("rate_us", new Dictionary<string, object>{
+            {"show_reason", RateUsManager.Instance.RateUsSource},
+            {"rate_result", rateResult}
+        });
+        RateUsManager.Instance.RateUsSource = null;
     }
 }
